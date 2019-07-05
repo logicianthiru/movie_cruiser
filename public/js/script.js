22d7d0ca0d47e3c1003b3617e351453f24231fce
logicianthiru <cutelovelythiru@gmail.com>
2019-07-05 18:09:49 +0530

    Final Working Assignment

--- public/js/script.js
+++ public/js/script.js
@@ -12,14 +12,13 @@ function getMovies() {
 			let body_html = "";
 			movie.forEach(element => {
 				body_html += `
-				<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card">
+				<div class="col-lg-6">
+				<div class="card card-space">
 				<img src='${ element.posterPath}' class='card-img-top' alt='${element.title}' height='400px'>
 				<div class="card-body">
 				<h5 class="card-title">${ element.title}</h5>
-				<h6 class="card-title">${ element.genre }</h6>
-				<p class='card-text'>${ element.description }</p>
-				<button onclick='addFavourite()' class='btn btn-primary'>Add to favourites</button>
+				<h6 class="card-title">${ element.genre}</h6>
+				<button onclick='addFavourite(${ element.id})' class='btn btn-primary'>Add to favourites</button>
 				</div>
 				</div>
 				</div>`
@@ -48,14 +47,12 @@ function getFavourites() {
 			let body_html = "";
 			favourite.forEach(element => {
 				body_html += `
-				<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card">
-				<img src='${ element.posterPath }' class='card-img-top' alt='${element.title}' height='400px'>
+				<div class="col-lg-6">
+				<div class="card card-space">
+				<img src='${ element.posterPath}' class='card-img-top' alt='${element.title}' height='400px'>
 				<div class="card-body">
 				<h5 class="card-title">${ element.title}</h5>
-				<h6 class="card-title">${ element.genre }</h6>
-				<p class='card-text'>${ element.description }</p>
-				<button onclick='addFavourite()' class='btn btn-primary'>Add to favourites</button>
+				<h6 class="card-title">${ element.genre}</h6>
 				</div>
 				</div>
 				</div>`
@@ -70,9 +67,45 @@ function getFavourites() {
 
 }
 
-function addFavourite() {
+function addFavourite(id) {
 
-	window.alert("Hello World.")
+	let favourite_movie;
+
+	var url = "http://localhost:3000/movies/" + id;
+	fetch(url, {
+		"method": "get",
+		"Content-Type": {
+			"headers": "application/json"
+		}
+	})
+		.then((response) => {
+			if (response.status == 200) {
+				return response.json();
+			}
+		})
+		.then((favourite) => {
+			//console.log(favourite);
+			favourite_movie = favourite;
+			fetch('http://localhost:3000/favourites', {
+				"method": "post",
+				"body": JSON.stringify(favourite_movie),
+				"headers": {
+					"Content-Type": "application/json"
+				}
+			})
+				.then((response) => {
+					if (response.status == 201) {
+						console.log("Record added successfully.");
+						getFavourites();
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				})
+		})
+		.catch((error) => {
+			console.log(error);
+		})
 
 }
 
